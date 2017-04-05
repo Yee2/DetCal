84e9db14084ff3436e3feaed5f5390afd81aef65
wu <boy404@163.com>
2017-04-05 17:07:10 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+		<router-view name='default'></router-view>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app{
+  max-width: 650px;
+  margin: 0 auto;
+  background-color: #EEE;
+  min-height: 100vh;
+}
+</style>
